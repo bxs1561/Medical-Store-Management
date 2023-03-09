b71b75af79df89d9954e4e41dd20b11f36a684a7
bikramsubedi <bxs1561@g.rit.edu>
2023-03-09 15:51:24 -0500

    start react frontend

---
+++ Frontend/medicalfrontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+// import './App.css';
+import Login from "./pages/Login";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+            </Routes>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
